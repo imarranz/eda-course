680bccf17bdc47bc6ef266525c2a8eab83767af2
imarranz <ibon.martinez@imarranz.com>
2020-02-08 23:34:43 +0100

    Código con ejemplos de la función shapiro(x)

---
+++ scripts/shapiro_example_01.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from numpy.random import normal
+from scipy.stats import shapiro
+from IPython.display import display, Markdown
+
+x = normal(loc = 2, scale = 1, size = 100)
+
+plt.hist(x, rwidth = 0.8)
+plt.show()
+w, p = shapiro(x)
+display(Markdown("W: {:.2f}\t statistic: {:.4f}".format(w,p)))
